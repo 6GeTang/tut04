e496cbae3641210ee220a4ec75e863abde5be8b1
6GeTang <112616302+6GeTang@users.noreply.github.com>
2022-10-01 10:49:08 +0800

    Update error

--- src/test/java/Task1/TestCalc.java
+++ src/test/java/Task1/TestCalc.java
@@ -10,5 +10,11 @@ public class TestCalc {
         int result = calc.add(1,2);
         assertEquals(3,result);
     }
+    @Test
+    public void testSubtract(){
+        Calc calc = new Calc();
+        int result2 = calc.subtract(4,2);
+        assertEquals(2,result2);
+    }
 
 }
